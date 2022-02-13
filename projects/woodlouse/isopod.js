6ab5e08c84edf04c1c2f1cd564c61b799c847d3c
castropointer <jonas.k.delarosa@gmx.de>
2022-02-13 21:50:45 +0100

    knocked everything down, starting afresh

---
+++ projects/woodlouse/isopod.js
@@ -0,0 +1,25 @@
+var showFeed=d=>{
+    let channel=d.getElementsByTagName('channel')[0];
+    let title=channel.getElementsByTagName('title')[0].innerHTML;
+    let author=channel.getElementsByTagName('itunes:author')[0].innerHTML;
+    let description=channel.getElementsByTagName('description')[0].innerHTML;
+    let image=channel.getElementsByTagName('image')[0].getElementsByTagName('url')[0].innerHTML;
+    document.getElementById('title').innerText=title;
+    document.getElementById('author').innerText=author;
+    //document.getElementById('desc').innerText=description;
+    document.getElementById('cover').style.backgroundImage='url('+image+')';
+}
+
+var loadPod=(url)=>{
+    fetch(url)
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => {showFeed(data)});
+}
+
+
+const sysk = 'https://feeds.megaphone.fm/stuffyoushouldknow';
+const fem = 'https://anchor.fm/s/4d413e38/podcast/rss';
+const daily = 'https://feeds.simplecast.com/54nAGcIl';
+
+loadPod(sysk);
